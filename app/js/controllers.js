9976f69a9408816b1bf92eb339e707f81e8e5992
Hannah Nordgren <hey@hereshannahs.info>
2014-10-22 12:49:57 -0400

    got side by side chat working

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,49 @@
 var MyControllers = angular.module('MyControllers', []);
 
 MyControllers.controller('MainCtrl', function($scope){});
+
+
+MyControllers.factory('items', function () {
+    var items = {};
+    items.data = [{
+        message: "Hello!"
+    }];
+    return items;
+});
+
+MyControllers.factory('things', function () {
+    var items = {};
+    items.data = [{
+        message: "Hello!"
+    }];
+    return items;
+});
+
+
+MyControllers.controller('ItemsController', function($scope, items){
+  $scope.items = items;
+
+  $scope.addItem = function (index) {
+    items.data.push({
+        message: $scope.newItemName
+    });
+  }
+
+});
+
+MyControllers.controller('ThingsController', function($scope, things){
+  $scope.things = things;
+
+  $scope.addThings = function (index) {
+    things.data.push({
+        message: $scope.newThingName
+    });
+  }
+
+});
+
+
+
+
+
+
